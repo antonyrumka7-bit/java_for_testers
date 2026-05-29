cf2fdbeb10cbb3234862db14ea8fe5e92ea6444e
Antony_test <antonyrumka7@gmail.com>
2026-05-29 18:27:30 +0300

    Окончание 1 урока

--- sandbox/src/main/java/ru/stqa/geometry/geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/geometry.java
@@ -5,9 +5,9 @@ import ru.stqa.geometry.figures.Square;
 
 public class geometry {
     public static void main(String[] args) {
-        Square.printSquareArea(7.0);
-        Square.printSquareArea(5.0);
-        Square.printSquareArea(3.0);
+        Square.printSquareArea(new Square(7.00));
+        Square.printSquareArea(new Square(5.0));
+        Square.printSquareArea(new Square(3.0));
 
         Rectangle.printRectanglArea(3.0 , 5.0);
         Rectangle.printRectanglArea(7.0, 9.0);
---
+++ sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.geometry.figures;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SquareTests {
+    @Test
+    void canCulculateArea() {
+        var s = new Square(5.0);
+        double result = s.area();
+        Assertions.assertEquals(25.0, result);
+    }
+
+    @Test
+    void canCulculatePerimetr() {
+        Assertions.assertEquals(20.0, new Square(5.0).perimetr());
+    }
+}
